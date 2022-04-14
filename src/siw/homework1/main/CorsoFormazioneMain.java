e1640ef3a00fc44fcb5c46bfdecee84e6c5e9ea5
lorenzo <lorenzocelli.2000@gmail.com>
2022-04-14 18:03:25 +0200

    main created and persistence tested

--- src/siw/homework1/main/CorsoFormazioneMain.java
+++ src/siw/homework1/main/CorsoFormazioneMain.java
@@ -1,5 +1,17 @@
 package siw.homework1.main;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class CorsoFormazioneMain {
 
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("homework1-unit");
+		EntityManager em = emf.createEntityManager();
+		em.close();
+		emf.close();
+	}
+	
 }
